c82eb6c9e928a0f69ccf62d2186d22a0e8e1085d
Alexandre Guénette <aguenette@pointlaz.com>
2025-05-20 17:10:29 -0400

    add updateCounterAndTimerControlConfig method

--- include/avt_vimba_camera/avt_vimba_camera.h
+++ include/avt_vimba_camera/avt_vimba_camera.h
@@ -190,6 +190,7 @@ private:
   void updateGPIOConfig(Config& config);
   void updateUSBGPIOConfig(Config& config);
   void updateIrisConfig(Config& config);
+  void updateCounterAndTimerControlConfig(Config& config);
 
 };
 }  // namespace avt_vimba_camera
