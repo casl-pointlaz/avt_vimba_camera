551bb66f626682649ea4edbc5567e552cf1d98fe
Alexandre Guénette <aguenette@pointlaz.com>
2025-05-26 13:59:30 -0400

    rename and clean updateControlAndTimerControlConfig method

--- src/avt_vimba_camera.cpp
+++ src/avt_vimba_camera.cpp
@@ -812,7 +812,7 @@ void AvtVimbaCamera::updateConfig(Config& config)
   updatePixelFormatConfig(config);
   updateAcquisitionConfig(config);
   updateIrisConfig(config);
-  updateCounterAndTimerControlConfig(config);
+  updateCounterControlConfig(config);
   config_ = config;
   ROS_INFO("--------------------------config done");
 }
@@ -1251,83 +1251,30 @@ void AvtVimbaCamera::updateUSBGPIOConfig(Config& config)
   }
 }
 
-void AvtVimbaCamera::updateCounterAndTimerControlConfig(Config& config)
+void AvtVimbaCamera::updateCounterControlConfig(Config& config)
 {
-  VmbUInt32_t counterDuration;
-  getFeatureValue("CounterDuration", counterDuration);
-  ROS_INFO_STREAM("CounterDuration: " << counterDuration);
-  setFeatureValue("CounterDuration", std::numeric_limits<VmbUInt32_t>::max());
+  // Controls the period of time until a CounterEnd event is generated, the
+  // CounterActive signal becomes inactive, and the counter is stopped.
+  setFeatureValue("CounterDuration", std::numeric_limits<VmbInt32_t>::max());
 
-  std::string counterEventActivation;
-  getFeatureValue("CounterEventActivation", counterEventActivation);
-  ROS_INFO_STREAM("CounterEventActivation: " << counterEventActivation);
+  // Selects the edge type of the electrical signal related to the event defined by
+  // CounterEventSource to increment the counter.
   setFeatureValue("CounterEventActivation", "RisingEdge");
 
-  std::string counterEventSource;
-  getFeatureValue("CounterEventSource", counterEventSource);
-  ROS_INFO_STREAM("CounterEventSource: " << counterEventSource);
+  // Selects the event to increment the counter.
   setFeatureValue("CounterEventSource", "Line0");
 
-  std::string counterResetActivation;
-  getFeatureValue("CounterResetActivation", counterResetActivation);
-  ROS_INFO_STREAM("CounterResetActivation: " << counterResetActivation);
-  setFeatureValue("CounterResetActivation", "RisingEdge");
-
-  std::string counterResetSource;
-  getFeatureValue("CounterResetSource", counterResetSource);
-  ROS_INFO_STREAM("CounterResetSource: " << counterResetSource);
+  // Selects the event to reset the counter.
   setFeatureValue("CounterResetSource", "Off");
 
-  std::string counterSelector;
-  getFeatureValue("CounterSelector", counterSelector);
-  ROS_INFO_STREAM("CounterSelector: " << counterSelector);
+  // Selects the counter to configure.
   setFeatureValue("CounterSelector", "Counter0");
 
-  std::string counterStatus;
-  getFeatureValue("CounterStatus", counterStatus);
-  ROS_INFO_STREAM("CounterStatus: " << counterStatus);
-
-  std::string counterTriggerActivation;
-  getFeatureValue("CounterTriggerActivation", counterTriggerActivation);
-  ROS_INFO_STREAM("CounterTriggerActivation: " << counterTriggerActivation);
+  // Selects the electrical signal level of the trigger to activate the counter.
   setFeatureValue("CounterTriggerActivation", "RisingEdge");
 
-  std::string counterTriggerSource;
-  getFeatureValue("CounterTriggerSource", counterTriggerSource);
-  ROS_INFO_STREAM("CounterTriggerSource: " << counterTriggerSource);
+  // Selects the event to trigger the counter.
   setFeatureValue("CounterTriggerSource", "Line0");
-
-  VmbInt32_t counterValue;
-  getFeatureValue("CounterValue", counterValue);
-  ROS_INFO_STREAM("CounterValue: " << counterValue);
-
-  VmbInt32_t counterValueAtReset;
-  getFeatureValue("CounterValueAtReset", counterValueAtReset);
-  ROS_INFO_STREAM("CounterValueAtReset: " << counterValueAtReset);
-
-  VmbFloat_t timerDelay;
-  getFeatureValue("TimerDelay", timerDelay);
-  ROS_INFO_STREAM(": " << timerDelay);
-
-  VmbFloat_t timerDuration;
-  getFeatureValue("TimerDuration", timerDuration);
-  ROS_INFO_STREAM(": " << timerDelay);
-
-  std::string timerSelector;
-  getFeatureValue("TimerSelector", timerSelector);
-  ROS_INFO_STREAM("TimerSelector: " << timerSelector);
-
-  std::string timerStatus;
-  getFeatureValue("TimerStatus", timerStatus);
-  ROS_INFO_STREAM("TimerStatus: " << timerStatus);
-
-  std::string timerTriggerActivation;
-  getFeatureValue("TimerTriggerActivation", timerTriggerActivation);
-  ROS_INFO_STREAM("TimerTriggerActivation: " << timerTriggerActivation);
-
-  std::string timerTriggerSource;
-  getFeatureValue("TimerTriggerSource", timerTriggerSource);
-  ROS_INFO_STREAM("TimerTriggerSource: " << timerTriggerSource);
 }
 
 
